d80862db6e20880f378c3a73c023075d8a403d20
ketlhin <ketlhin_penitente@hotmail.com>
2021-04-09 01:36:22 -0300

    sql

---
+++ SqlDatabaseSetup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+
+namespace Agenda {
+    [TestClass()]
+    public class SqlDatabaseSetup {
+
+        [AssemblyInitialize()]
+        public static void InitializeAssembly(TestContext ctx) {
+            // Setup the test database based on setting in the
+            // configuration file
+            SqlDatabaseTestClass.TestService.DeployDatabaseProject();
+            SqlDatabaseTestClass.TestService.GenerateData();
+        }
+
+    }
+}
